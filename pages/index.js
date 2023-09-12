445f8d67bf40d8d04b0e117a52bb22d836751afd
trumancodes <trumanlahr@gmail.com>
2023-09-12 02:21:34 -0700

    favicon and manifest

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,12 @@ export default function Home() {
   return (
     <Layout>
       <Head>
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#6b66ff" />
+        <meta name="theme-color" content="#6b66ff" />
         <title>{siteTitle}</title>
       </Head>
       <Hero />
